d70998829e5f252c78d4d1d06fc43e30edabc668
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-04-18 12:15:25 -0300

    first commit

---
+++ admin/service/client-service.js
@@ -0,0 +1,30 @@
+const criaNovaLinha = (nome, email, id) => {
+  const linhaNovoCliente = document.createElement('tr');
+  const conteudo = `
+        <td class="td" data-td>${nome}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="tabela__botoes-controle">
+                <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
+                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+            </ul>
+        </td>`;
+  linhaNovoCliente.innerHTML = conteudo;
+  return linhaNovoCliente;
+};
+
+const tabela = document.querySelector('[data-tabela]');
+
+const http = new XMLHttpRequest();
+
+http.open('GET', 'http://localhost:3000/profile');
+
+http.onload = () => {
+  const data = http.response;
+  const clientes = JSON.parse(data);
+  clientes.forEach((cliente) => {
+    tabela.appendChild(criaNovaLinha(cliente.nome, cliente.email, cliente.id));
+  });
+};
+
+http.send();
