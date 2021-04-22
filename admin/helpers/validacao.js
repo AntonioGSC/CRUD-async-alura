efdde61bacff4d0d519290740302a2fcd35ba57e
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-04-22 19:15:32 -0300

    CPF validator

--- admin/helpers/validacao.js
+++ admin/helpers/validacao.js
@@ -43,11 +43,25 @@ const mensagensErro = {
     valueMissing: 'O campo CPF não pode estar vazio.',
     customError: 'O CPF digitado não é válido.',
   },
+  cep: {
+    valueMissing: 'O campo CEP não pode estar vazio.',
+    patternMismatch: 'O CEP digitado não é válido',
+  },
+  logradouro: {
+    valueMissing: 'O campo Logradouro não pode estar vazio.',
+  },
+  cidade: {
+    valueMissing: 'O campo Cidade não pode estar vazio.',
+  },
+  estado: {
+    valueMissing: 'O campo Estado não pode estar vazio.',
+  },
 };
 
 const validadores = {
   dataNascimento: (input) => validaDataNascimento(input),
   cpf: (input) => validaCPF(input),
+  cep: (input) => recuperarCEP(input),
 };
 
 const mostraMensagemErro = (tipoInput, input) => {
@@ -83,10 +97,10 @@ const maiorDeIdade = (data) => {
 };
 
 const validaCPF = (input) => {
-  const cpfFormatado = input.value.replace('/D/g', '');
+  const cpfFormatado = input.value.replace(/\D/g, '');
   let mensagem = '';
 
-  if (!checaCPFRepetido(cpfFormatado)) {
+  if (!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
     mensagem = 'O CPF digitado não é válido';
   }
   input.setCustomValidity(mensagem);
@@ -120,8 +134,32 @@ const checaEstruturaCPF = (cpf) => {
   return checaDigitoVerificador(cpf, multiplicador);
 };
 
+const checaDigitoVerificador = (cpf, multiplicador) => {
+  if (multiplicador >= 12) {
+    return true;
+  }
+
+  let multiplicadorInicial = multiplicador;
+  let soma = 0;
+  const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('');
+  const digitoVerificador = cpf.charAt(multiplicador - 1);
+
+  for (let i = 0; multiplicadorInicial > 1; multiplicadorInicial--) {
+    soma = soma + cpfSemDigitos[i] * multiplicadorInicial;
+    i++;
+  }
+
+  if (digitoVerificador == confirmaDigito(soma)) {
+    return checaDigitoVerificador(cpf, multiplicador + 1);
+  }
+
+  return false;
+};
+
 const confirmaDigito = (soma) => {
   return 11 - (soma % 11);
 };
 
-const checaDigitoVerificador = (cpf, multiplicador) => {};
+const recuperarCEP = (input) => {
+  const cep = input.value.replace(/\D/g, '');
+};
